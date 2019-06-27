393062cdb17b7b8446675457741e6397725d33f5
ThomLebel <thom.lebel@orange.fr>
2019-06-27 19:19:59 +0200

    testing Forge Networking

---
+++ MagicSchool/Assets/Bearded Man Studios Inc/Test/BasicGameLogic.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BeardedManStudios.Forge.Networking.Unity;
+using UnityEngine;
+
+public class BasicGameLogic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		NetworkManager.Instance.InstantiatePlayerCube();
+    }
+}
